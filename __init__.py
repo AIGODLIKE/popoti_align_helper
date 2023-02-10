6d0789d756396f5ef58cd609644ea3a640b3b2a2
EMM <3209970865@qq.com>
2023-02-10 10:17:42 +0800

    change translate.py to localdb

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-
-from . import icons, ops, panel, preferences,translate
+from . import icons, ops, panel, preferences, localdb
 
 bl_info = {
     "name": "POPOTI Align Helper",
@@ -16,7 +15,7 @@ mod_tuple = (
     ops,
     icons,
     panel,
-    translate,
+    localdb,
     preferences,
 )
 
