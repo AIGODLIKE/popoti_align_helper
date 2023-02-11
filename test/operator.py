d857279a61db40a4847a4a46970d6bafaa2f798d
EMM <3209970865@qq.com>
2023-02-11 14:58:06 +0800

    add test folder

---
+++ test/operator.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class OperatorTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_operator(self):
+        ...
+
+    def test_property(self):
+        ...
