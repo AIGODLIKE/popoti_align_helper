a2199663785330e2c355e024a5bc4eeef977fbc3
EMM <3209970865@qq.com>
2023-02-13 15:05:34 +0800

    change pie menu idname and layout

--- pie.py
+++ pie.py
@@ -1,7 +1,7 @@
 import bpy
 from bpy.types import Menu
 
-from .panel import set_axis
+from .panel import set_axis, draw_ground, draw_cursor_active_original, draw_distribution, draw_center_align
 from .utils import screen_relevant_direction_3d_axis
 
 
@@ -13,15 +13,23 @@ class AlignPieMenu(Menu):
         layout = self.layout
 
         pie = layout.menu_pie()
-        (x, x_), (y, y_) = screen_relevant_direction_3d_axis(context)
+        (x, x_), (y, y_) = direction = screen_relevant_direction_3d_axis(context)
         set_axis(pie, {x_}, 'Align_Left')
         set_axis(pie, {x}, 'Align_Right')
         set_axis(pie, {y_}, 'Align_Down')
         set_axis(pie, {y}, 'Align_Up')
-        pie.column().label(text="a")
-        pie.column().label(text="b")
-        pie.column().label(text="c")
-        pie.column().label(text="d")
+
+        col = pie.column()
+        draw_distribution(col, direction)
+
+        col = pie.column()
+        draw_center_align(col, direction)
+
+        pie.separator()
+
+        col = pie.column(align=True)
+        draw_ground(col)
+        draw_cursor_active_original(col)
 
 
 def register():
